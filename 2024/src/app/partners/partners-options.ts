6aae112caeeafd43043d4fba5a08afa4edbfaeb5
FredericoBresani <fbs4@cin.ufpe.br>
2024-05-07 00:36:30 -0300

    Design improvements

--- 2024/src/app/partners/partners-options.ts
+++ 2024/src/app/partners/partners-options.ts
@@ -1,11 +1,59 @@
-export const partnersOptions: { img_address: string }[] = [
-    /*{
-        img_address: '../../../assets/images/logo-manguezal.png',
-    },
-    .
-    .
-    .
+export const partnersOptions: { img_address: string, type: string }[] = [
     {
-        img_address: '../../../assets/images/logo-manguezal.png',
-    }*/
+        img_address: '../../../assets/images/CESAR-01.png',
+        type: 'tubarao'
+    },
+    {
+        img_address: '../../../assets/images/Citi-01.png',
+        type: 'chie'
+    },
+    {
+        img_address: '../../../assets/images/Neurotech-01.png',
+        type: 'chie',
+
+    },
+    {
+        img_address: '../../../assets/images/Porto-Digital-01.png',
+        type: 'guaiamum',
+    },
+    {
+        img_address: '../../../assets/images/Sebrae-01.png',
+        type: 'chie'
+    },
+    {
+        img_address: '../../../assets/images/brain.png',
+        type: 'aratu',
+    },
+    {
+        img_address: '../../../assets/images/arsenal.png',
+        type: 'aratu',
+    },
+    {
+        img_address: '../../../assets/images/cubo.png',
+        type: 'aratu',
+    },
+    {
+        img_address: '../../../assets/images/daus.png',
+        type: 'chie',
+    },
+    {
+        img_address: '../../../assets/images/happen.png',
+        type: 'aratu',
+    },
+    {
+        img_address: '../../../assets/images/inover.png',
+        type: 'aratu',
+    },
+    {
+        img_address: '../../../assets/images/medstaff.png',
+        type: 'aratu',
+    },
+    {
+        img_address: '../../../assets/images/quark.png',
+        type: 'aratu',
+    },
+    {
+        img_address: '../../../assets/images/spencer.png',
+        type: 'aratu',
+    },
 ];
